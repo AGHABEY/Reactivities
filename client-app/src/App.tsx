8cdea3c7a8f17084eb8ba6d21250ae2b5b089a97
AGHABEY <rafet.agabeyli98@gmail.com>
2021-09-10 01:12:50 +0400

    Commit1

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ducks } from './demo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {ducks.map(duck=>
+          <div key={duck.name}>
+            <span>{duck.name}</span>
+
+          <button onClick={()=> duck.makeSound(duck.name+' Salam')}>Make Sound</button>
+          </div>
+          )}
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. gedey gezey Selyani Selyani
         </p>
         <a
           className="App-link"
