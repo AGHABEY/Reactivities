ddb5088803ded14b9da6a04daefc0fa6d3bd690a
AGHABEY <rafet.agabeyli98@gmail.com>
2021-09-12 14:38:25 +0400

    new futures added

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ducks } from './demo';
+import axios from 'axios';
+
 
 function App() {
+
+  const[activities, setActivities]=useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/activities').then(response =>{
+      console.log(response);
+      setActivities(response.data);
+    })
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck=>
-          <div key={duck.name}>
-            <span>{duck.name}</span>
-
-          <button onClick={()=> duck.makeSound(duck.name+' Salam')}>Make Sound</button>
-          </div>
-          )}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. gedey gezey Selyani Selyani
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ul>
+          {activities.map((activity : any) => (
+            <li key={activity.id}>
+                {activity.title}
+            </li>
+          ))}
+        </ul>
+      </header> 
     </div>
   );
 }
