6b710701bb0d9b424c5f89a7d8fdb4389d558bd6
AGHABEY <rafet.agabeyli98@gmail.com>
2021-09-28 15:15:56 +0400

    last changes

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,29 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity'
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/ActivityDashboard';
+
+function App() {
+
+  const[activities, setActivities]=useState<Activity[]>([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/activities').then(response =>{
+      console.log(response);
+      setActivities(response.data);
+    })
+  },[])
+
+  return (
+    < >
+     <NavBar/>
+     <Container style={{marginTop:'7em'}}>
+      <ActivityDashboard activities={activities}/>
+      </Container>
+    </>
+  );
+}
+
+export default App;
